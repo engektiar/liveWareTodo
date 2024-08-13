0d6a265f29d083ce63f4e86fa95cd9d97e9834c3
Ekitar <eng.ektiar@gmail.com>
2024-08-13 12:17:39 +0600

    from submit

--- resources/views/livewire/first-component.blade.php
+++ resources/views/livewire/first-component.blade.php
@@ -1,10 +1,10 @@
 <div>
      <h1>This is live ware </h1>
 
- <form action="">
+ <form  wire:submit="createNewTodo" action="">
    <label for="">Type Your todo</label><br><br>
    <input wire:model="todo_item" type="text"><br><br>
-   <button wire:click.prevent="createNewTodo">Create New Todo</button>
+   <button>Create New Todo</button>
  </form>
 
 
